f5970bfc6ebb8b4ca7ed788f248a86353519316b
pranathimallavarapu18-gif <pranathimallavarapu18@gmail.com>
2026-01-10 17:01:52 +0530

    Add files via upload

---
+++ 10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main()
+{
+ int a, b, c, max;
+printf("Enter three numbers: ");
+ scanf("%d %d %d", &a, &b, &c);
+ max = a > b ? ( a > c ? a : c) : ( b > c ? b : c);
+
+printf("Largest among the three numbers is %d", max); 
+return 0;
+}
+
